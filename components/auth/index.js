8c1188606c08ccff6ee2f86bae85f74f5b7173a8
Edd Tsao <etphonehomies@gmail.com>
2017-01-11 01:30:07 -0800

    Move passport config to auth

--- components/auth/index.js
+++ components/auth/index.js
@@ -1,5 +1,14 @@
 // @flow
 
+let passport = require('passport')
+let LocalStrategy = require('passport-local').Strategy
+
+passport.use(new LocalStrategy((username, password, done) => {
+  return done(null, {
+    name: 'Bob'
+  })
+}))
+
 let router = require('express').Router()
 
 router.use(require('./login'))
--- passport.config.js
+++
@@ -1,8 +0,0 @@
-let passport = require('passport')
-let LocalStrategy = require('passport-local').Strategy
-
-passport.use(new LocalStrategy((username, password, done) => {
-  return done(null, {
-    name: 'Bob'
-  })
-}))
